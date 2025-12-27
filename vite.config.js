d7d2709fec465eca60c8da6917cabba45669d4ab
hhhhe731 <2090179996@qq.com>
2025-12-27 14:06:10 +0800

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import WindiCSS from 'vite-plugin-windicss'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(),WindiCSS()],
-  base:process.env.NODE_ENV==='production'?'/weatherlite/':'/'
-})
